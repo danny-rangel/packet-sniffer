0a622ab46db6d6d2ba187d71bfc9616e0f348f81
Danny Rangel <danielrangel.tech@gmail.com>
2019-06-03 23:31:49 -0700

    added function for mac address formatting

--- sniffer.py
+++ sniffer.py
@@ -7,3 +7,7 @@ def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_address(dest_mac), get_mac_address(src_mac), socket.htons(proto), data[14:]
 
+# function that returns formatted MAC address
+def get_mac_address(bytes_address):
+    bytes_str = map('{:02x}'.format, bytes_address)
+    return ':'.join(bytes_str).upper()
