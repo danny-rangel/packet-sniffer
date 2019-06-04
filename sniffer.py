6e02264e330a78316ad2193411158a4ab09d5e05
Danny Rangel <danielrangel.tech@gmail.com>
2019-06-03 23:27:55 -0700

    added function for unpacking ethernet frame

---
+++ sniffer.py
@@ -0,0 +1,9 @@
+import socket
+import struct
+import textwrap
+
+# function for unpacking the ethernet frame
+def ethernet_frame(data):
+    dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
+    return get_mac_address(dest_mac), get_mac_address(src_mac), socket.htons(proto), data[14:]
+
