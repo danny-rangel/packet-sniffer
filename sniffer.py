329b1d2f34e1bc88af4a44c87e8219ade8fe0e43
Danny Rangel <danielrangel.tech@gmail.com>
2019-06-04 00:43:59 -0700

    added format func

--- sniffer.py
+++ sniffer.py
@@ -63,4 +63,15 @@ def udp_segment(data):
     src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
     return src_port, dest_port, size, data[8:]
 
+
+# formatting function
+def format_lines(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
+        
+
 main()
