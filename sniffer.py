aa8736a195b219ef06d7816dd7620c83421b6669
Danny Rangel <danielrangel.tech@gmail.com>
2019-06-03 23:40:34 -0700

    added main function

--- sniffer.py
+++ sniffer.py
@@ -2,6 +2,18 @@ import socket
 import struct
 import textwrap
 
+
+
+def main():
+    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+    while True:
+        raw_data, address = conn.recvfrom(65536)
+        dest_mac, src_mac, ethernet_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, ethernet_proto))
+
+
 # function for unpacking the ethernet frame
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
